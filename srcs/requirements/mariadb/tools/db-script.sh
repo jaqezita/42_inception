67678f471a65f8442dc4863f7e4cfe406cd7dbc2
jaqezita <jaque_hp@hotmail.com>
2024-07-19 20:34:00 -0300

    update

--- srcs/requirements/mariadb/tools/db-script.sh
+++ srcs/requirements/mariadb/tools/db-script.sh
@@ -2,8 +2,7 @@
 
 service mariadb start
 
-mariadb -u root -e \
-	"CREATE DATABASE IF NOT EXISTS ${WP_DB}; \
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${WP_DB}; \
 	CREATE USER '${WP_USER}'@'%' IDENTIFIED BY '${WP_PW}'; \
 	GRANT ALL ON ${WP_DB}.* TO '${WP_USER}'@'%' IDENTIFIED BY '${WP_PW}'; \
 	FLUSH PRIVILEGES;"
